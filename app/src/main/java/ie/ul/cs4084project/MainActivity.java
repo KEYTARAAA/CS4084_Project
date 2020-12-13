927f2d593e6139f3568044197c5edee57f142b4f
KITARA STEWART <18241476@studentmail.ul.ie>
2020-12-13 12:51:38 +0000

    Permission change

--- app/src/main/java/ie/ul/cs4084project/MainActivity.java
+++ app/src/main/java/ie/ul/cs4084project/MainActivity.java
@@ -2,8 +2,12 @@ package ie.ul.cs4084project;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -38,7 +42,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         storageReference = FirebaseStorage.getInstance().getReference().child("profilesInfo");
-        ;
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION }
+        ,PackageManager.PERMISSION_GRANTED);
+        }
         setContentView(R.layout.activity_main);
     }
 
